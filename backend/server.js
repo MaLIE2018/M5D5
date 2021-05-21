a4f1ace7fd2deaa19dede550cbddaeceb200f130
abdul2112 <abdulkadir.s.salad1@gmail.com>
2021-05-21 12:11:02 +0100

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,13 @@
-import express from "express"
-import cors from "cors"
+import express from 'express';
+import cors from 'cors';
+import productRoutes from './routes/products/products.js';
 
+const app = express();
+const port = 3001;
 
-const app = express()
-const port = 3001
+app.use(cors());
+app.use(express.json());
 
+app.use('/products', productRoutes);
 
-
-app.use(cors())
-app.use(express.json())
-
-app.use("/products", productRoutes, fileRoutes)
-
-app.listen(port, () => console.log(`Server at ${port}`))
+app.listen(port, () => console.log(`Server at ${port}`));
