7c4c43f1117c2d333e3a9347013380e056ce0cb5
magdakolaniak <magda.kolaniak@gmail.com>
2021-05-21 14:28:17 +0200

    reviews routes

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,23 @@
-import express from "express"
-import cors from "cors"
+import express from 'express';
+import cors from 'cors';
+import reviewsRoutes from './routes/reviews/reviews.js';
+import {
+  badRequestErrorHandler,
+  notFoundErrorHandler,
+  catchAllErrorHandler,
+} from './routes/errorsHandler.js';
 
+const app = express();
+const port = 3001;
 
-const app = express()
-const port = 3001
+app.use(cors());
+app.use(express.json());
 
+// app.use('/products', productRoutes, fileRoutes);
+app.use('/reviews', reviewsRoutes);
 
+app.use(badRequestErrorHandler);
+app.use(notFoundErrorHandler);
+app.use(catchAllErrorHandler);
 
-app.use(cors())
-app.use(express.json())
-
-app.use("/products", productRoutes, fileRoutes)
-
-app.listen(port, () => console.log(`Server at ${port}`))
+app.listen(port, () => console.log(`Server at ${port}`));
